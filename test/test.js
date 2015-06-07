0349ad7f58ecb115c3d7d68c8fd02ed24a90f616
Alejandro <alemures@hotmail.com>
2015-06-08 00:07:46 +0100

    Buffer fully supported, added a flag to the raw payload

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var port = 5000;
 var server = new Server();
 server.listen(port);
 server.on('connection', function(socket) {
-	socket.emit('data', 'Hello, Clients!');
+	socket.emit('data', new Buffer('Hello, Clients!'));
 });
 
 var Socket = require('../index').Socket;
@@ -12,5 +12,5 @@ var socket = new Socket({
 	port: 5000
 });
 socket.on('data', function(data) {
-	console.log(data);
+	console.log(data.toString());
 });
