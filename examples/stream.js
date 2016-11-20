4e2c404d9e5879e40708a7c349a02a6e035d20f8
Alejandro <alemures@hotmail.com>
2016-11-20 04:26:03 +0000

    New example added about streams

--- examples/stream.js
+++ examples/stream.js
@@ -1,29 +1,13 @@
 'use strict';
 
-var Readable = require('stream').Readable;
-var Writable = require('stream').Writable;
+var fs = require('fs');
 
 var Server = require('../index').Server;
 
 var server = new Server();
 server.on('connection', function (socket) {
-  socket.on('streaming_dashes', function (info, readStream) {
-    console.log('Starting streaming info: ', info);
-
-    var writeStream = new Writable({
-      write: function (chunk, encoding, cb) {
-        setTimeout(function () {
-          console.log('Chunk received of', chunk.length, 'bytes');
-          cb();
-        }, 100);
-      }
-    });
-
-    writeStream.on('finish', function () {
-      console.log('Streaming finished');
-    });
-
-    readStream.pipe(writeStream);
+  socket.on('image', function (info, readStream) {
+    readStream.pipe(fs.createWriteStream(info.name));
   });
 });
 
@@ -35,25 +19,6 @@ var socket = new Socket({
   port: 5000
 });
 
-var longString = new Array(100001).join('-');
-
-var counter = 0;
-var max = 25;
-var readStream = new Readable({
-  read: function (size) {
-    var _this = this;
-
-    if (counter++ < max) {
-      setTimeout(function () {
-        _this.push(longString);
-      }, 50);
-    } else {
-      this.push(null);
-    }
-  }
-});
-
 socket.on('connect', function () {
-  readStream.pipe(socket.stream('streaming_dashes',
-      { size: (longString.length * max) + ' bytes' }));
+  fs.createReadStream('img.jpg').pipe(socket.stream('image', { name: 'img.jpg' }));
 });
