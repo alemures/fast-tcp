c98aa17ecb1f5871cf91463c3b5374b19e143cca
Alejandro Santiago <alemures@hotmail.com>
2018-02-22 17:36:34 +0100

    Added more tests

--- test/unit/serializer.js
+++ test/unit/serializer.js
@@ -31,7 +31,7 @@ describe('Serializer', () => {
       const serializer = new Serializer();
       const spy = sinon.spy(Serializer, '_serialize');
 
-      const datas = ['data', 5, null, Buffer.alloc(0), {}, true];
+      const datas = ['data', 5, 5.2, null, Buffer.alloc(0), {}, true, undefined];
 
       datas.forEach((data) => {
         serializer.serialize('event', data, Serializer.MT_DATA, 1);
@@ -39,10 +39,12 @@ describe('Serializer', () => {
 
       expect(spy.calledWith('event', datas[0], Serializer.MT_DATA, 1, Serializer.DT_STRING)).to.be.true;
       expect(spy.calledWith('event', datas[1], Serializer.MT_DATA, 1, Serializer.DT_INTEGER)).to.be.true;
-      expect(spy.calledWith('event', datas[2], Serializer.MT_DATA, 1, Serializer.DT_EMPTY)).to.be.true;
-      expect(spy.calledWith('event', datas[3], Serializer.MT_DATA, 1, Serializer.DT_BINARY)).to.be.true;
-      expect(spy.calledWith('event', serializer._objectSerializer(datas[4]), Serializer.MT_DATA, 1, Serializer.DT_OBJECT)).to.be.true;
-      expect(spy.calledWith('event', datas[5] ? 1 : 0, Serializer.MT_DATA, 1, Serializer.DT_BOOLEAN)).to.be.true;
+      expect(spy.calledWith('event', datas[2], Serializer.MT_DATA, 1, Serializer.DT_DECIMAL)).to.be.true;
+      expect(spy.calledWith('event', datas[3], Serializer.MT_DATA, 1, Serializer.DT_EMPTY)).to.be.true;
+      expect(spy.calledWith('event', datas[4], Serializer.MT_DATA, 1, Serializer.DT_BINARY)).to.be.true;
+      expect(spy.calledWith('event', serializer._objectSerializer(datas[5]), Serializer.MT_DATA, 1, Serializer.DT_OBJECT)).to.be.true;
+      expect(spy.calledWith('event', datas[6] ? 1 : 0, Serializer.MT_DATA, 1, Serializer.DT_BOOLEAN)).to.be.true;
+      expect(spy.calledWith('event', null, Serializer.MT_DATA, 1, Serializer.DT_EMPTY)).to.be.true;
     });
   });
 });
